16752390a2a5e5d2f962296346c40a948735e241
Alex Booker <alexbooker@fastmail.im>
2016-06-07 20:56:57 +0100

    Refactor connect() into smaller functions

--- index.js
+++ index.js
@@ -2,34 +2,41 @@ const Sequelize = require('sequelize')
 const pify = require('pify')
 const glob = pify(require('glob'))
 
+const associateModels = (models) => {
+  Object.keys(models).forEach(model => {
+    if (models[model].associate) {
+      models[model].associate(models)
+    }
+  })
+}
+
+const reduceModels = (modelPaths) => {
+  return modelPaths.reduce((models, modelPath) => {
+    const createModel = require(modelPath)
+    if (typeof createModel !== 'function') {
+      throw new Error(`Found a model named ${modelPath} but it does not export a function.`)
+    }
+    const model = createModel(context)
+    if (!model) {
+      throw new Error(`Found a model named ${modelPath}. It exports a function but that function doesn't return anything.`)
+    }
+    return Object.assign(models, {
+      [model.name]: model
+    })
+  }, { })
+}
+
 const context = {
   Sequelize,
-
   connect (modelsGlob, ...sequelizeArgs) {
     this.connection = new Sequelize(...sequelizeArgs)
     return glob(modelsGlob).then(modelPaths => {
       if (modelPaths.length < 1) {
         throw new Error(`Could not find any models in ${modelsGlob}.`)
       }
-      this.models = modelPaths.reduce((models, modelPath) => {
-        const createModel = require(modelPath)
-        if (typeof createModel !== 'function') {
-          throw new Error(`Found a model named ${modelPath} but it does not export a function.`)
-        }
-        const model = createModel(this)
-        if (!model) {
-          throw new Error(`Found a model named ${modelPath}. It exports a function but that function doesn't return anything.`)
-        }
-        return Object.assign(models, {
-          [model.name]: model
-        })
-      }, { })
+      this.models = reduceModels(modelPaths)
     }).then(() => {
-      Object.keys(this.models).forEach(model => {
-        if (this.models[model].associate) {
-          this.models[model].associate(this.models)
-        }
-      })
+      associateModels(this.models)
       return this.connection.sync()
     })
   }
